0eeb3e276a75321fb82b3721da42e1bb6d6105cf
Chris <clj@ollure.com>
2024-07-26 15:19:56 -0500

    mobile version still show tect error

--- components/ImageViewer.js
+++ components/ImageViewer.js
@@ -2,6 +2,7 @@ import { StyleSheet, Image } from 'react-native';
 
 export default function ImageViewer({placeholderImageSource, selectedImage }) {
 	const imageSource = selectedImage ? {uri: selectedImage} : placeholderImageSource;
+ 
   return <Image source={imageSource} style={styles.image} />;
 }
 
